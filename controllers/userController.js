b8a4befbc801d2b27ae566da1c2b79d83136fd43
shanmugapriya1203 <priyamuthukumar1203@gmail.com>
2023-10-18 21:54:04 +0530

    Follow and Unfollow Api functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -77,5 +77,30 @@ export const loginUser = async (req, res) => {
   }
 
   export const followUnFollowUser= async(req,res)=>{
+try {
+    const {id}=req.params;
+    const userToModify= await User.findById(id);
+    const currentUser= await User.findById(req.user._id)
+    if(id===req.user._id) return res.status(400).json({message:"You cannot follow or unfollow yourself"})
+if(!userToModify || !currentUser){
+    return res.status(400).json({message:"User not found"})
+}
+const isFollowing= currentUser.following.includes(id)
+if(isFollowing){
+await User.findByIdAndUpdate(req.user._id,{$pull:{following:id}})
+await User.findByIdAndUpdate(id,{$pull:{followers:req.user._id}})
+res.status(200).json({message:"User unfollowed successfully"})
+}
+else{
+  //Follow User
+  await User.findByIdAndUpdate(req.user._id,{$push:{following:id}})
+await User.findByIdAndUpdate(id,{$push:{followers:req.user._id}})
+res.status(200).json({message:"User followed successfully"})
+}
 
+
+} catch (error) {
+    res.status(500).json({ message: error.message });
+    console.log("Error in Follow and Unfollow User", error.message);
+}
   }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,12 @@
 import express from "express"
 import {  followUnFollowUser, loginUser, logoutUser, signupUser } from "../controllers/userController.js"
+import protectRoute from '../middleware/protectRoute.js'
 const router= express.Router()
 
 
 router.post('/signup',signupUser)
 router.post('/login',loginUser)
 router.post('/logout',logoutUser)
-router.post('/follow',followUnFollowUser)
+router.post('/follow/:id',protectRoute,followUnFollowUser)
 
 export default router
