1c55a313c8f51bbb5b7cf9808a5692a06ff06f81
shanmugapriya1203 <priyamuthukumar1203@gmail.com>
2023-10-24 09:06:58 +0530

    complete createPost api

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,3 +1,38 @@
-export const createPost=async(req,res)=>{
+import Post from "../models/postModel.js";
+import User from "../models/userModel.js";
+
+export const createPost = async (req, res) => {
+  try {
+    const { postedBy, text, img } = req.body;
+
+    if (!postedBy || !text) {
+      return res.status(400).json({ message: "postedBy and text fields are required" });
+    }
+
+    const user = await User.findById(postedBy);
+
+    if (!user) {
+      return res.status(404).json({ message: "User Not found" });
+    }
+
+    if (user._id.toString() !== req.user._id.toString()) {
+      return res.status(401).json({ message: "Unauthorized to create post" });
+    }
+
+    const maxLength = 500;
+    if (text.length > maxLength) {
+      return res.status(400).json({ message: `Text must be less than ${maxLength} characters` });
+    }
+
+    const newPost = new Post({
+      postedBy, text, img
+    });
+
+    await newPost.save();
     
-}
+    return res.status(201).json(newPost); 
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,5 +1,8 @@
 import express from 'express';
-import { createPost } from '../controllers/postController';
+import { createPost } from '../controllers/postController.js';
+import protectRoute from './../middleware/protectRoute.js';
 const router= express.Router();
 
-router.post('/create',createPost)
+router.post('/create',protectRoute,createPost)
+
+export default router
