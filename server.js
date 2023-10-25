4e1a840f02f99bba5a91fd12f4a6cf6c243bc69d
shanmugapriya1203 <priyamuthukumar1203@gmail.com>
2023-10-25 09:38:44 +0530

    install cors and body-parser

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './db/connectDb.js'
 import cookieParser from 'cookie-parser'
+import cors from 'cors';
+import bodyParser from 'body-parser'; 
 import userRoutes from './routes/userRoutes.js'
 import postRoutes from './routes/postRoutes.js'
 dotenv.config()
@@ -10,7 +12,11 @@ connectDB()
 const app=express()
 
 const PORT= process.env.PORT || 5000
+app.use(cors());
 
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json())
 app.use(cookieParser())
 
