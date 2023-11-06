9f646651aae082244ec68d86d60f617e1e281ef1
shanmugapriya1203 <priyamuthukumar1203@gmail.com>
2023-11-06 12:26:39 +0530

    update data according to deletePost

--- controllers/postController.js
+++ controllers/postController.js
@@ -67,6 +67,10 @@ export const deletePost= async(req,res)=>{
         if(post.postedBy.toString() !== req.user._id.toString()){
 return res.status(401).json({error:"Unauthorized to delete post"})
     }   
+    if(post.img){
+      const imgId= post.img("/").pop().split(".")[0];
+      await cloudinary.uploader.destroy(imgId)
+    }
       await Post.findByIdAndDelete(req.params.id)
       res.status(200).json({message:"Post Deleted Successfuly"})
  }
